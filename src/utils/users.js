e6c7906d6f168f5b322becc67fbe0a3baee4fd1b
Nicolaas Steenekamp <nicolaas.steenekamp@gmail.com>
2022-01-27 16:11:32 +0200

    dude

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,48 +1,49 @@
-const users = []
+const users = [];
 
 const addUser = ({ id, username, room }) => {
-    username = username.trim().toLowerCase()
-    room = room.trim().toLowerCase()
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    if (!username || !room) {
-        return {
-            error: 'Username and room are required!'
-        }
-    }    const existingUser = users.find((user) => {
-        return user.room === room && user.username === username
-    })
+  if (!username || !room) {
+    return {
+      error: "Username and room are required!",
+    };
+  }
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
 
-    if (existingUser) {
-        return {
-            error: 'Username is in use!'
-        }
-    }
+  if (existingUser) {
+    return {
+      error: "Username is in use!",
+    };
+  }
 
-    const user = { id, username, room }
-    users.push(user)
-    return { user }
-}
+  const user = { id, username, room, letters: ["a", "b", "c"] };
+  users.push(user);
+  return { user };
+};
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id)
+  const index = users.findIndex((user) => user.id === id);
 
-    if (index !== -1) {
-        return users.splice(index, 1)[0]
-    }
-}
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
 
 const getUser = (id) => {
-    return users.find((user) => user.id === id)
-}
+  return users.find((user) => user.id === id);
+};
 
 const getUsersInRoom = (room) => {
-    room = room.trim().toLowerCase()
-    return users.filter((user) => user.room === room)
-}
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room === room);
+};
 
 module.exports = {
-    addUser,
-    removeUser,
-    getUser,
-    getUsersInRoom
-}
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
